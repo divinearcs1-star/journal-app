2fa968f57949b7d01f818d8ef01c0e664756cf54
divinearcs1-star <divinearcs1@gmail.com>
2026-08-03 01:11:25 +0530

    dokcer

--- src/main/java/com/firstproject/journalApp/config/SwaggerConfig.java
+++ src/main/java/com/firstproject/journalApp/config/SwaggerConfig.java
@@ -34,5 +34,6 @@ public class SwaggerConfig {
                                 .in(SecurityScheme.In.HEADER)
                                 .name("Authorization")
                 ));
+        
     }
 }
